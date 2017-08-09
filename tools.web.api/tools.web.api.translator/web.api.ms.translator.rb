41d71537ec949d3a20d69a66b8291398eef666b3
Yasuhiro Yamamoto <yasuhiro.y0807@gmail.com>
2017-08-09 13:49:40 +0900

    - reformat config.yml files. - refactoring ms translator of web api.

--- tools.web.api/tools.web.api.translator/web.api.ms.translator.rb
+++ tools.web.api/tools.web.api.translator/web.api.ms.translator.rb
@@ -11,77 +11,6 @@ require 'json'
 require 'nokogiri'
 require 'optparse'
 
-#Usin by Microsoft Translate API
-class Translation
-    #Constructor method
-    def initialize(args = {})
-        @cache = {} # hash type
-        @token_uri = args[:token_uri]
-        @token_key = args[:key]
-        @api_uri = args[:api_uri]
-        @proxy = args[:proxy]
-    end
-
-    def getAccessToken()
-        res = nil
-        uri = URI.parse(@token_uri)
-        Net::HTTP.version_1_2
-        #    https = Net::HTTP::Proxy(PROXY_SERVER_URI, PROXY_PORT, PROXY_USER, PROXY_PASS).new(uri.host, uri.port)
-        https = Net::HTTP.new(uri.host, uri.port)
-        https.use_ssl = true
-        https.verify_mode = OpenSSL::SSL::VERIFY_NONE
-        req = Net::HTTP::Post.new(uri.path)
-        req['Ocp-Apim-Subscription-Key']=@token_key
-        res = https.request(req)
-        if res.message == "OK"
-            @updateTime = Time.now
-            @token = res.body
-        else
-            raise "access token acquisition failure"
-        end
-    end
-
-    def existsTransCache(word)
-        @cache.has_key?(word)
-    end
-
-    def setTransCache(word, resultWord)
-        @cache[word] = resultWord
-    end
-
-    def getTransCache(word)
-        @cache[word]
-    end
-
-    def do(word, src, dest)
-        return getTransCache(word) if existsTransCache(word)
-
-        token = getAccessToken
-        uri = URI.parse(@api_uri)
-        Net::HTTP.version_1_2
-        # http = Net::HTTP::Proxy(PROXY_SERVER_URI, PROXY_PORT, PROXY_USER, PROXY_PASS).new(uri.host, uri.port)
-        https = Net::HTTP.new(uri.host, uri.port)
-        params = {
-            :text => word,
-            :from => src,
-            :to => dest
-        }
-        query_string = params.map{ |k,v|
-            URI.encode(k.to_s) + "=" + URI.encode(v.to_s)
-        }.join("&")
-        req = Net::HTTP::Get.new(uri.path + "?" + query_string)
-        req['Authorization'] = "Bearer #{token}"
-        res = https.request(req)
-        result = nil
-        if res.message == "OK"
-            doc = Nokogiri::XML.parse(res.body)
-            result = doc.root.content
-            setTransCache(word, result)
-        end
-        result
-    end
-end
-
 class Hash
     def symbolize_keys!
         t=self.dup
@@ -91,11 +20,89 @@ class Hash
     end
 end
 
-stdargs = ARGV.getopts('kp', 'key:nil', 'proxy:nil')
-config = YAML.load(File.read("config.yml"))
-config.update(stdargs).symbolize_keys!
-config[:dest].each do |dest|
-    FileUtils.mkdir_p("dest\/" + dest)
-    ms_api = Translation.new(config)
-    p ms_api.do("Hello", config[:src], dest)
+module WebAPI
+
+    class MSTranslator
+        def initialize(args = {})
+            @cache = {}
+            @token_uri = URI.parse(args[:token_uri]) if args[:token_uri] != nil
+            @api_uri = URI.parse(args[:api_uri]) if args[:api_uri] != nil
+            @proxy = URI.parse(args[:proxy]) if args[:proxy] != nil
+            @token_key = args[:key]
+        end
+
+        def getAccessToken()
+            res = nil
+            Net::HTTP.version_1_2
+
+            https = (@proxy == nil ? Net::HTTP.new(@token_uri.host, @token_uri.port) : Net::HTTP::Proxy(@proxy.host, @proxy.port, @proxy.user, @proxy.password).new(@token_uri.host, @token_uri.port))
+            https.use_ssl = true
+            https.verify_mode = OpenSSL::SSL::VERIFY_NONE
+            https.open_timeout = 3
+            https.read_timeout = 5
+            
+            req = Net::HTTP::Post.new(@token_uri.path)
+            req.add_field("Content-Length", 2048)
+            req.add_field("Content-Type", "application/json")
+            req.add_field("Accept", "application/jwt")
+            req.add_field("Ocp-Apim-Subscription-Key", @token_key)
+
+            begin 
+                res = https.request(req)
+                if res.message == "OK"
+                    @updateTime = Time.now
+                    @token = res.body
+                else
+                    raise "access token acquisition failure"
+                end
+            rescue Timeout::Error
+                raise "connection error, and timeout error."
+            rescue e
+                raise "fatal error."
+            end
+        end
+
+        def do(word, src, dest)
+            return getTransCache(word) if existsTransCache(word)
+            res = nil
+            translated = nil
+            token = getAccessToken
+
+            Net::HTTP.version_1_2
+            https = (@proxy == nil ? Net::HTTP.new(@api_uri.host, @api_uri.port) : Net::HTTP::Proxy(@proxy.host, @proxy.port, @proxy.user, @proxy.password).new(@api_uri.host, @api_uri.port))
+            https.open_timeout = 3
+            https.read_timeout = 5
+            params = { :text => word, :from => src, :to => dest }
+            query_string = params.map{ |k,v| URI.encode(k.to_s) + "=" + URI.encode(v.to_s) }.join("&")
+            req = Net::HTTP::Get.new(uri.path + "?" + query_string)
+            req['Authorization'] = "Bearer #{token}"
+
+            begin 
+                res = https.request(req)
+                if res.message == "OK"
+                    translated = Nokogiri::XML.parse(res.body).root.content
+                    setTransCache(word, translated)
+                end
+                return translated
+            rescue Timeout::Error
+                raise "connection and timeout error."
+            rescue e
+                raise "fatal error."
+            end
+        end
+
+        def existsTransCache(word)
+            @cache.has_key?(word)
+        end
+
+        def setTransCache(word, resultWord)
+            @cache[word] = resultWord
+        end
+
+        def getTransCache(word)
+            @cache[word]
+        end
+
+    end
+
 end
